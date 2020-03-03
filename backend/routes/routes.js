d523be1a2e375e4de3022e79fb493049f2d2c5d1
David Taing <adavidtaing@gmail.com>
2020-03-03 21:01:12 +1100

    Init Routes (Not Yet Implemented)

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,14 +1,17 @@
 const routes = (app) => {
+    // Get all books
     app.route('/books')
         .get((req, res) => {})
 
     app.route('/book')
-        .get((req, res) => {})
+        // Add or update book
         .post((req, res) => {})
-        .delete((req, res) => {})
 
-    app.route('/user/:_id')
+    app.route('/book/:isbn')
+        // Get book by isbn
         .get((req, res) => {})
+        // Delete book by isbn
+        .delete((req, res) => {})
 }
  
 export default routes;
